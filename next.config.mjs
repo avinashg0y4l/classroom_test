4655f39114e54b31bc97a70403a8fe8b5a5134b2
avinashg0y4l <avinashg0y4l@gmail.com>
2025-09-06 19:44:28 +0530

    fix(config): enable ESLint error ignoring during production builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // <-- ignore ESLint errors/warnings during production build
+  },
+};
 
 export default nextConfig;
